9c08df5c7e4cd6326c4e8d5c9169184d8c594288
Brandon Raphael <brandonraphael77@gmail.com>
2026-06-03 14:08:25 +0700

    fixed missing comma

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -58,7 +58,7 @@ pub fn run() -> tauri::Result<()> {
             projects::workspace_terminal_panel_create,
             projects::workspace_terminal_snapshot_delete,
             projects::workspace_terminal_snapshot_get,
-            projects::workspace_terminal_snapshot_save
+            projects::workspace_terminal_snapshot_save,
             source_control::source_control_commit,
             source_control::source_control_discard_path,
             source_control::source_control_discard_paths,
