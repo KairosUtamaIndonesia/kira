b7ab1a1a6160ba8ac43b80b267c4ac69e24a9939
Brandon Raphael <brandonraphael77@gmail.com>
2026-06-08 14:05:27 +0700

    refactor(admin): adopt shadcn sidebar shell

--- apps/admin/features/admin-shell/components/AdminShell.tsx
+++ apps/admin/features/admin-shell/components/AdminShell.tsx
@@ -1,5 +1,9 @@
 import type { ReactNode } from "react";
 
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+import { AdminBreadcrumbProvider } from "./AdminBreadcrumbs";
 import { AdminHeader } from "./AdminHeader";
 import { AdminSidebar } from "./AdminSidebar";
 
@@ -9,13 +13,17 @@ type AdminShellProperties = {
 
 function AdminShell({ children }: AdminShellProperties) {
   return (
-    <div className="grid min-h-svh bg-background text-foreground lg:grid-cols-[18rem_minmax(0,1fr)]">
-      <AdminSidebar />
-      <div className="flex min-w-0 flex-col">
-        <AdminHeader />
-        <main className="flex-1 p-4 md:p-6">{children}</main>
-      </div>
-    </div>
+    <TooltipProvider>
+      <AdminBreadcrumbProvider>
+        <SidebarProvider>
+          <AdminSidebar />
+          <SidebarInset>
+            <AdminHeader />
+            <main className="flex-1 p-4 md:p-6">{children}</main>
+          </SidebarInset>
+        </SidebarProvider>
+      </AdminBreadcrumbProvider>
+    </TooltipProvider>
   );
 }
 
